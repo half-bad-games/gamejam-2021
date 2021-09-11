ddd64afbb61dda7001fe502228ee7ddb62c90679
jayveedee <jayveedees@gmail.com>
2021-09-11 20:05:13 +0200

    implemented some player functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,15 @@ using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Camera camera;
     [SerializeField] public float movementSpeed;
     [SerializeField] public float health;
+    [SerializeField] public int size;
+    [SerializeField] public int currentXPToLevel;
+    [SerializeField] public int currentXP;
+    [SerializeField] public int currentSP;
     private Stats stats;
+    private float t = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,7 @@ public class Player : MonoBehaviour
         var comp = new DecoratorFactory(p).generate(10);
         stats = comp.extend();
         health = stats.health;
+        
     }
 
     // Update is called once per frame
@@ -22,6 +29,16 @@ public class Player : MonoBehaviour
     {
         // GameObject.Find("HEALTH").GetComponent<UnityEngine.UI.Text>().text = ((int)health).ToString();
         HandlePlayerMovement();
+        HandleStayInsideScreen();
+        if (Input.GetKey("space"))
+        {
+            IncreaseSize(1);
+        }
+
+        if (camera.orthographicSize < size)
+        {
+            IncreaseCameraSize(camera.orthographicSize);
+        }
     }
 
     void HandlePlayerMovement()
@@ -58,4 +75,78 @@ public class Player : MonoBehaviour
         
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, player_angle + 90));
     }
+    
+    void HandleStayInsideScreen()
+    {
+        var bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var topRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight));
+        
+        var cameraRect = new Rect(
+            bottomLeft.x,
+            bottomLeft.y,
+            topRight.x - bottomLeft.x,
+            topRight.y - bottomLeft.y);
+        
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, cameraRect .xMin + 3, cameraRect .xMax - 3),
+            Mathf.Clamp(transform.position.y, cameraRect .yMin + 3, cameraRect .yMax - 3),
+            transform.position.z);
+    }
+    
+    private void IncreaseCameraSize(float camSize)
+    {
+        t += Time.deltaTime;
+        camera.orthographicSize = camSize * 5;
+    }
+
+    void IncreaseXPToleven()
+    {
+        currentXPToLevel *= (int) 1.2f;
+    }
+
+    void IncreaseCurrentXP(int xpGains)
+    {
+        currentXP += xpGains;
+        if (currentXP >= currentXPToLevel)
+        {
+            IncreaseXPToleven();
+            currentXP = currentXP - currentXPToLevel;
+            IncreaseSP();
+        }
+    }
+
+    void IncreaseSP()
+    {
+        currentSP += 1;
+    }
+
+    void DecreaseSP(int amount)
+    {
+        currentSP -= amount;
+        if (currentSP > 0)
+        {
+            currentSP = 0;
+        }
+    }
+
+    void IncreaseSize(int amount)
+    {
+        Vector3 local = transform.localScale;
+        //sets the local scale of an object
+        transform.localScale = new Vector3(local.x + 0.2f * amount,local.y + 0.2f * amount,local.z + 0.2f * amount);
+        size += amount;
+        // if (movementSpeed <= 50 && movementSpeed > 1)
+        // {
+        //     movementSpeed -= amount * 0.2f;
+        // } 
+        // else if (movementSpeed <= 0)
+        // {
+        //     movementSpeed = 0.2f;
+        // }
+        // else
+        // {
+        //     movementSpeed -= amount;
+        // }
+    }
+    
 }
