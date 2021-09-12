5fe53b950a6caf9810e70f13457361a94ae2c889
kruserr <46799551+kruserr@users.noreply.github.com>
2021-09-12 18:46:24 +0100

    final3

---
+++ Assets/Scripts/EnemyDecoratorFactory.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class EnemyDecoratorFactory
+{
+    Component component;
+    dynamic baseComponent;
+    List<Decorator> decorators;
+
+    public EnemyDecoratorFactory(Component component, dynamic baseComponent)
+    {
+        this.component = component;
+        this.baseComponent = baseComponent;
+        
+        this.decorators = new List<Decorator>();
+
+        decorators.Add(new MouthDecorator());
+        decorators.Add(new BasicFinsDecorator());
+        decorators.Add(new EyesDecorator());
+        decorators.Add(new WhiskersDecorator());
+        decorators.Add(new SlimeDecorator());
+        decorators.Add(new SpearDecorator());
+        decorators.Add(new SpikesDecorator());
+        decorators.Add(new ThirdEyeDecorator());
+        decorators.Add(new FishTailDecorator());
+    }
+
+    // Generate a composition of random of Decorators
+    public Decorator generate(int n)
+    {
+        Decorator dec = null;
+
+        for (var i = 0; i < n + 1; i++)
+        {
+            var lastDec = dec;
+            dec = System.Activator.CreateInstance(
+                decorators[Random.Range(0, this.decorators.Count)].GetType()
+            ) as Decorator;
+
+            if (i == 0)
+            {
+                dec.setComponent(this.component);
+            }
+            else
+            {
+                dec.setComponent(lastDec, this.baseComponent);
+            }
+        }
+
+        return dec;
+    }
+}
