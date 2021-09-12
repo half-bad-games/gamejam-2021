167b59e1911fc068f655f27a96ed0348e89288c0
jayveedee <jayveedees@gmail.com>
2021-09-12 14:05:29 +0200

    asdf

---
+++ Assets/Scripts/PlayerCenterGravity.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerCenterGravity : MonoBehaviour
+{
+    private bool isPlayer;
+    private float thrust = 100;
+    private float torque = 100;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetInstanceID() != GameObject.Find("PlayerContainer").gameObject.GetInstanceID())
+        {
+            isPlayer = false;
+            Vector2 thrust = new Vector2(Random.Range(-this.thrust, this.thrust), Random.Range(-this.thrust, this.thrust));
+            float torque = Random.Range(-this.torque, this.torque);
+        
+            rb.AddForce(thrust);
+            rb.AddTorque(torque);
+        }
+        else
+        {
+            isPlayer = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer)
+        {
+            var modelPosition = transform.position;
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            var delta_x = modelPosition.x - mousePosition.x;
+            var delta_y = modelPosition.y - mousePosition.y;
+
+            var delta_x_norm = delta_x / (Mathf.Sqrt(delta_x * delta_x + delta_y * delta_y));
+            var delta_y_norm = delta_y / (Mathf.Sqrt(delta_x * delta_x + delta_y * delta_y));
+            var player_angle = (Mathf.Atan2(delta_y_norm, delta_x_norm) * (Mathf.Rad2Deg));
+
+            // GameObject parent = transform.parent.gameObject;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, player_angle + 90));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("AM TRIGGERING");
+        Player player;
+        Enemy enemy;
+        if (isPlayer)
+        {
+            player = gameObject.GetComponent<Player>();
+            enemy = other.gameObject.GetComponent<Enemy>();
+            Debug.Log("player" + player + " enemy " + enemy);
+            if (player.size > enemy.size)
+            {
+                player.IncreaseCurrentXP(enemy.xpGains);
+                Destroy(other.gameObject);
+            }
+        }
+        else
+        {
+            
+        }
+    }
+}
